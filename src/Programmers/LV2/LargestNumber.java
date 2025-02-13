f6e2d3acd43671ec0b8190de2441572ff860f78b
YouYoungSun <ffdr803@naver.com>
2025-02-13 16:28:51 +0900

    Merge remote-tracking branch 'origin/master'

--- src/Programmers/LV2/LargestNumber.java
+++ src/Programmers/LV2/LargestNumber.java
@@ -1,12 +1,34 @@
 package Programmers.LV2;
 
+import java.util.*;
+
 public class LargestNumber {
-    public static String solution(int[] numbers){
+    public static String solution(int[] numbers) {
+        String[] strNumbers = new String[numbers.length];
+        for(int i =0;i<numbers.length;i++){
+            strNumbers[i] = String.valueOf(numbers[i]);
+        }
 
+        Arrays.sort(strNumbers, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return (b+a).compareTo(a+b);
+            }
+        });
 
-        return "";
+        StringBuilder sb = new StringBuilder();
+        for(String str : strNumbers){
+            sb.append(str);
+        }
+
+        if(sb.charAt(0)=='0'){
+            return "0";
+        }
+        return sb.toString();
     }
     public static void main(String[] args) {
         int[] numbers = {6,10,2};
+        System.out.println(solution(numbers));
     }
 }
+
